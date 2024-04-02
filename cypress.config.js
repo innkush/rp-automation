3be2cb46f2d3e0453cb5556a4e497125ffed274d
Inna Kulish <inna_kulish@epam.com>
2024-04-02 13:53:03 +0200

    fixed yml file

--- cypress.config.js
+++ cypress.config.js
@@ -33,6 +33,19 @@ module.exports = defineConfig({
       require('cypress-mochawesome-reporter/plugin')(on);
       require('@cypress/code-coverage/task')(on, config);
       require('@cypress/grep/src/plugin')(config);
+      on('before:browser:launch', (browser, launchOptions) => {
+        console.log('before launching browser')
+        console.log(browser)
+
+        if (browser.name === 'chrome') {
+          // https://www.ghacks.net/2013/10/06/list-useful-google-chrome-command-line-switches/
+          launchOptions.args.push('--window-size=1920,1080')
+
+          console.log('chrome launch args:')
+          console.log(launchOptions.args.join(os.EOL))
+          return launchOptions
+        }
+      })
       return config
     }
   }

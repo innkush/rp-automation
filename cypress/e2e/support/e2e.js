55c17b7d8a0929682732cca2694a74732544e631
Inna Kulish <inna_kulish@epam.com>
2024-04-17 15:47:01 +0200

    added api call to generate data

--- cypress/e2e/support/e2e.js
+++ cypress/e2e/support/e2e.js
@@ -17,7 +17,38 @@ beforeEach(() => {
     throw new Error("URL is not defined");
   }
 });
-before(() => {});
+
+before(() => {
+  const username = Cypress.env("RP_USERNAME");
+  const password = Cypress.env("RP_PASSWORD");
+
+  cy.request({
+    method: "POST",
+    url: "http://127.0.0.1:8080/uat/sso/oauth/token",
+    body: `grant_type=password&username=${username}&password=${password}`,
+    headers: {
+      accept: "application/json, text/plain, */*",
+      authorization: "Basic dWk6dWltYW4=",
+      "content-type": "application/x-www-form-urlencoded",
+    },
+  }).then((resp) => {
+    expect(resp.status).to.eq(200);
+    const accessToken = resp.body.access_token;
+    cy.request({
+      method: "POST",
+      url: "http://127.0.0.1:8080/api/v1/demo/superadmin_personal/generate",
+      body: "{}",
+      headers: {
+        accept: "application/json, text/plain, */*",
+        authorization: `bearer ${accessToken}`,
+        "content-type": "application/json",
+      },
+    }).then((resp) => {
+      expect(resp.status).to.eq(200);
+      expect(resp.body).to.have.property("launchIds");
+    });
+  });
+});
 
 afterEach(() => {
   cy.log("Closing browser window");
